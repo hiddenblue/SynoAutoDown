fcb72b66d240e412cd4c10d305f70ad436cf7720
chase <freewayrong@foxmail.com>
2023-02-15 16:34:22 +0800

    reconstruct the synoatuo with Python requests.session, which make it more convenient. automaticlly login and save cookies. no requirements to copy cookies and syno-token from browsers.

--- SynoAuto.py
+++ SynoAuto.py
@@ -1,46 +1,43 @@
-# coding:utf-8
-# author: hiddenblue
-# 一个调用群晖download station 下载文件的api
-
+import json
 import requests
 import re
-import json
 from time import sleep
 
 class SnoAuto(object):
 
-    @staticmethod # 因为在实例初始化时需要使用，所以不要定义成静态方法，不会自动传入cls。
-    def parse_raw_cookies(cookies: str) -> dict:
-        temp1 = cookies.split(sep="; ")
-        cookies_dict = {}
-        for element in temp1:
-            temp2 = element.split(sep="=")
-            key = temp2[0]
-            value = temp2[1]
-            cookies_dict[key] = value
-        return cookies_dict
-
-    def __init__(self, cookies: str, entry_url: str, header: dict, down_path:str):
-
-        self.entry_url = entry_url.strip()
-        self.header = header
-        self.cookies = self.parse_raw_cookies(cookies.strip())
+    def __init__(self, account: str, passwd: str, down_path:str):
         self.down_path = down_path
         # 默认的同时进行的最大任务数
         self.max_down_task = 2
         self.max_error_task = 3
+        self.session = requests.Session()
+        self.initiateSession(account, passwd)
         # 默认的最大错误任务数
         # self.check_payload = {'sort_by': "task_id",'order': "ASC",'action': "enum",'type': '["emule"]','type_inverse': 'true','limit': '25','additional': '["detail", "transfer"]','status': 'null','status_inverse': 'null','api': 'SYNO.DownloadStation2.Task','method': 'list','version': '2',}
-        print("successfully initiate the SnoAuto object")
+
+    #这一步是构建requests session 来初始化一个session对象，方便后面自动调用cookies，等参数
+    def initiateSession(self, account, passwd):
+        self.loginUrl = 'http://sustech.fun:5000/webapi/entry.cgi/'
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41'}
+        self.loginData  = {
+        'api': 'SYNO.API.Auth',
+        'version': '7',
+        'method': 'login',
+        'account': '%s'%account,
+        'passwd': '%s'%passwd}
+        self.session.headers = self.headers
+        initialResponse =  self.session.post(self.loginUrl, data= self.loginData)
+        print(initialResponse.status_code)
+        print(initialResponse.text)
+        if (initialResponse.status_code==200) and (json.loads(initialResponse.text).get("success")==True):
+            print("successfully initiate the SnoAuto session")
+        else:
+            print("sorry. Fail to initiate the SnoAuto session")
+            print("please check your network and login parameter")
 
     def connect(self, data: dict) -> str:
-
-        headers = self.header
-        cookies = self.cookies
-        url = self.entry_url
-        resonse = requests.post(url=url, headers=headers, cookies=cookies, data=data)
+        resonse = self.session.post(url=self.loginUrl, data=data)
         print(resonse.status_code)
-
         return resonse.text
 
     def get_task_list(self) -> tuple[dict, list]:
@@ -78,7 +75,7 @@ class SnoAuto(object):
         if len(filename) == 0:
             print("下载任务url %s无匹配下载文件名称")
         else:
-            if (str(tasklist).find(filename[0])==1):
+            if (str(tasklist).find(filename[0]) != -1):
                 print("下载任务url提取的文件名：%s在下载任务列表中已存在"%filename)
                 return True
             else:
@@ -91,7 +88,6 @@ class SnoAuto(object):
             return False
 
     def start_down(self, down_link: str):
-        cookies = self.cookies
         add_task_payload = {
             'type': "url",
             'destination': "%s" % self.down_path,
--- main.py
+++ main.py
@@ -2,27 +2,19 @@
 # author: hiddenblue
 # 一个调用群晖download station 下载文件的api
 
-
 from SynoAuto import SnoAuto
 
 down_list = [] #这里面导入你需要批量下载的文件的url，带双引号，组成Python列表
 
-# 直接复制群晖f12页面下方请求表头的cookies内容填入即可
-cookies = '这里填入你的浏览器cookies'
+account = "这里填入你的群晖用户名"
 
-entry_url = '这里填入你的群晖的地址' + '/webapi/entry.cgi'
-
-# 在下面填入群晖f12页面请求表头里面的X-SYNO-TOKEN值，比如.xxxxxx
-header = {
-    'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.78",
-    'X-Requested-With': 'XMLHttpRequest',
-    'X-SYNO-TOKEN': '.VuLDpcgVUEYU'}
+passwd = '这里填入你的群晖账户对应的密码'
 
 path = '这里填入你的下载路径' # 比如 download 或者 video等
 
-#群晖下载任务需要以上四个参数，缺一不可
+#初始化一个synoatuo实例对象
+snoauto = SnoAuto(account, passwd, path)
 
-snoauto = SnoAuto(cookies, entry_url, header, path)
-
+#然后就开始快速下载了
 for i in down_list:
     snoauto.synodown(i)
